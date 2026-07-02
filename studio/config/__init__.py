99556b107bc04c4efc49e192bb4c6734ae16c596
yogansbgm-web <honenben@gmail.com>
2026-07-02 13:31:37 +0000

    FIX: config/__init__.py only import from constants, not input_normalizer

--- studio/config/__init__.py
+++ studio/config/__init__.py
@@ -1,66 +1,20 @@
-# engine/__init__.py
-from .input_normalizer import normalize_to_pil, NormalizationError
-from .detector import (
-    get_detector,
-    BaseDetector,
-    MockDetector,
-    SmartMockDetector,
-    GeminiDetector,
-    OpenRouterVisionDetector,
-)
-from .gap_analyzer import GapAnalyzer
-from .creative_director import CreativeDirector
-from .prompt_compiler import (
-    PromptAdapter,
-    MidjourneyAdapter,
-    SDXLAdapter,
-    FluxAdapter,
-    KlingAdapter,
-    RunwayAdapter,
-    build_partial_prompt_vocabulary,
-)
-from .agnes import agnes_respond, agnes_suggest
-from .vpos_selector import (
-    TIME_MAP,
-    MOOD_MAP,
-    STYLE_MAP,
-    get_vpos_filters,
-    filter_vpos_by_user,
-    get_user_mode,
-)
-from .vpos_reference import get_reference
-from .vpos_mapper import map_vpos_to_dna, get_channel_match
-from .prompt_formula import compile_all_formulas, PromptFormula
+# config/__init__.py
+from .constants import *
 
 __all__ = [
-    "normalize_to_pil",
-    "NormalizationError",
-    "get_detector",
-    "BaseDetector",
-    "MockDetector",
-    "SmartMockDetector",
-    "GeminiDetector",
-    "OpenRouterVisionDetector",
-    "GapAnalyzer",
-    "CreativeDirector",
-    "PromptAdapter",
-    "MidjourneyAdapter",
-    "SDXLAdapter",
-    "FluxAdapter",
-    "KlingAdapter",
-    "RunwayAdapter",
-    "build_partial_prompt_vocabulary",
-    "agnes_respond",
-    "agnes_suggest",
-    "TIME_MAP",
-    "MOOD_MAP",
-    "STYLE_MAP",
-    "get_vpos_filters",
-    "filter_vpos_by_user",
-    "get_user_mode",
-    "get_reference",
-    "map_vpos_to_dna",
-    "get_channel_match",
-    "compile_all_formulas",
-    "PromptFormula",
+    "CHANNEL_NAMES",
+    "SUPPORTED_IMAGE_TYPES",
+    "DATA_DIR",
+    "MAX_IMAGE_SIZE_MB",
+    "MAX_IMAGE_PIXELS",
+    "DEFAULT_CONFIDENCE_THRESHOLD",
+    "EXPORT_FORMATS",
+    "DEFAULT_USER_AGENT",
+    "DEFAULT_TIMEOUT",
+    "LOG_LEVEL",
+    "LOG_FILE",
+    "VISION_MODEL",
+    "OPENROUTER_VISION_MODEL",
+    "OPENROUTER_API_KEY",
+    "GEMINI_API_KEY",
 ]
