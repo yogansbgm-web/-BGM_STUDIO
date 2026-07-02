a86f2274cf2dc3a98472b1de327cb73f65d94d4e
yogansbgm-web <honenben@gmail.com>
2026-07-02 12:54:48 +0000

    FIX: remove VisionDetector import, add correct detector classes

--- studio/engine/__init__.py
+++ studio/engine/__init__.py
@@ -1,14 +1,36 @@
 # engine/__init__.py
 from .input_normalizer import normalize_to_pil, NormalizationError
-from .detector import get_detector, BaseDetector, MockDetector, SmartMockDetector, VisionDetector
+from .detector import (
+    get_detector,
+    BaseDetector,
+    MockDetector,
+    SmartMockDetector,
+    GeminiDetector,
+    OpenRouterVisionDetector,
+)
 from .gap_analyzer import GapAnalyzer
 from .creative_director import CreativeDirector
-from .prompt_compiler import PromptAdapter, MidjourneyAdapter, SDXLAdapter, FluxAdapter, KlingAdapter, RunwayAdapter, build_partial_prompt_vocabulary
-from .prompt_formula import PromptFormula, compile_all_formulas, FORMULA_STRUCTURE
-from .vpos_selector import TIME_MAP, MOOD_MAP, STYLE_MAP, get_vpos_filters, filter_vpos_by_user, get_user_mode
-from .vpos_mapper import map_vpos_to_dna, get_channel_match
-from .vpos_reference import get_reference, REFERENCE_CHANNELS
+from .prompt_compiler import (
+    PromptAdapter,
+    MidjourneyAdapter,
+    SDXLAdapter,
+    FluxAdapter,
+    KlingAdapter,
+    RunwayAdapter,
+    build_partial_prompt_vocabulary,
+)
 from .agnes import agnes_respond, agnes_suggest
+from .vpos_selector import (
+    TIME_MAP,
+    MOOD_MAP,
+    STYLE_MAP,
+    get_vpos_filters,
+    filter_vpos_by_user,
+    get_user_mode,
+)
+from .vpos_reference import get_reference
+from .vpos_mapper import map_vpos_to_dna, get_channel_match
+from .prompt_formula import compile_all_formulas, PromptFormula
 
 __all__ = [
     "normalize_to_pil",
@@ -17,7 +39,8 @@ __all__ = [
     "BaseDetector",
     "MockDetector",
     "SmartMockDetector",
-    "VisionDetector",
+    "GeminiDetector",
+    "OpenRouterVisionDetector",
     "GapAnalyzer",
     "CreativeDirector",
     "PromptAdapter",
@@ -27,19 +50,17 @@ __all__ = [
     "KlingAdapter",
     "RunwayAdapter",
     "build_partial_prompt_vocabulary",
-    "PromptFormula",
-    "compile_all_formulas",
-    "FORMULA_STRUCTURE",
+    "agnes_respond",
+    "agnes_suggest",
     "TIME_MAP",
     "MOOD_MAP",
     "STYLE_MAP",
     "get_vpos_filters",
     "filter_vpos_by_user",
     "get_user_mode",
+    "get_reference",
     "map_vpos_to_dna",
     "get_channel_match",
-    "get_reference",
-    "REFERENCE_CHANNELS",
-    "agnes_respond",
-    "agnes_suggest",
+    "compile_all_formulas",
+    "PromptFormula",
 ]
